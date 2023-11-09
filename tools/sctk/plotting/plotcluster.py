cf44bf507e414f3682b613fa1e8d24722c9ba106
WeipengMO <windzlife@gmail.com>
2023-11-09 09:30:24 +0800

    add plotting function

---
+++ tools/sctk/plotting/plotcluster.py
@@ -0,0 +1,128 @@
+from typing import Optional
+from ..preprocessing import adata_process
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def percent_in_cluster(
+    adata,
+    groupby: str,
+    label: str,
+    labelColor: Optional[dict] = None,
+    needCounts=True,
+    ax=None,
+    plot_legend=True,
+    legend_kwargs={"bbox_to_anchor": [1, 1]},
+    swapAxes=False,
+):
+    """
+    Plot the percentage of each label in each groupby category.
+
+    Parameters
+    ----------
+    adata
+        Annotated data matrix.
+    groupby
+        The column name of the category.
+    label
+        The column name of the label.
+    labelColor
+        The color of each label.
+    needCounts
+        Whether to show the counts of each groupby category.
+    ax
+        The axes object to plot on.
+    plot_legend
+        Whether to plot the legend.
+    legend_kwargs
+        The keyword arguments for the legend.
+    swapAxes
+        Whether to swap the x-axis and y-axis.
+
+    Returns
+    -------
+    The axes object.
+
+    Examples
+    --------
+    >>> sctk.pl.percent_in_cluster(adata, 'leiden', 'batch')
+    """
+    if not labelColor:
+        labelColor = adata_process.get_adata_color(adata, label)
+
+    groupbyWithLabelCountsDf = (
+        adata.obs.groupby(groupby)[label].apply(lambda x: x.value_counts()).unstack()
+    )
+    groupbyWithLabelCounts_CumsumPercDf = groupbyWithLabelCountsDf.pipe(
+        lambda x: x.cumsum(1).div(x.sum(1), 0) * 100
+    )
+    legendHandleLs = []
+    legendLabelLs = []
+    if not swapAxes:
+        for singleLabel in groupbyWithLabelCounts_CumsumPercDf.columns[::-1]:
+            ax = sns.barplot(
+                x=groupbyWithLabelCounts_CumsumPercDf.index,
+                y=groupbyWithLabelCounts_CumsumPercDf[singleLabel],
+                color=labelColor[singleLabel],
+                ax=ax,
+            )
+            plt.sca(ax)
+            legendHandleLs.append(
+                plt.Rectangle(
+                    (0, 0), 1, 1, fc=labelColor[singleLabel], edgecolor="none", label=singleLabel
+                )
+            )
+            legendLabelLs.append(singleLabel)
+        legendHandleLs, legendLabelLs = legendHandleLs[::-1], legendLabelLs[::-1]
+        # plt.legend(legendHandleLs, legendLabelLs, frameon=False, **legend_kwargs)
+        if plot_legend:
+            plt.legend(handles=legendHandleLs, frameon=False, **legend_kwargs)
+
+        plt.xlabel(groupby.capitalize())
+        plt.ylabel(f"Percentage")
+        if needCounts:
+            for i, label in enumerate(groupbyWithLabelCounts_CumsumPercDf.index):
+                plt.text(
+                    i,
+                    105,
+                    f"$\it{{N}}$ = {adata[adata.obs[groupby] == label].shape[0]}",
+                    rotation=90,
+                    ha="center",
+                    va="bottom",
+                )
+    else:
+        for singleLabel in groupbyWithLabelCounts_CumsumPercDf.columns[::-1]:
+            ax = sns.barplot(
+                y=groupbyWithLabelCounts_CumsumPercDf.index,
+                x=groupbyWithLabelCounts_CumsumPercDf[singleLabel],
+                color=labelColor[singleLabel],
+                ax=ax,
+            )
+            plt.sca(ax)
+            legendHandleLs.append(
+                plt.Rectangle(
+                    (0, 0), 1, 1, fc=labelColor[singleLabel], edgecolor="none", label=singleLabel
+                )
+            )
+            legendLabelLs.append(singleLabel)
+
+        if plot_legend:
+            plt.legend(handles=legendHandleLs[::-1], frameon=False, **legend_kwargs)
+
+        plt.ylabel(groupby.capitalize())
+        plt.xlabel(f"Percentage")
+        if needCounts:
+            for i, label in enumerate(groupbyWithLabelCounts_CumsumPercDf.index):
+                plt.text(
+                    101,
+                    i,
+                    f"$\it{{N}}$ = {adata[adata.obs[groupby] == label].shape[0]}",
+                    rotation=0,
+                    ha="left",
+                    va="center",
+                )
+                
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+
+    return ax
