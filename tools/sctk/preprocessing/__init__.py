1a721604a75c2b1faea6a6b0fe48f9e2d9545228
WeipengMO <windzlife@gmail.com>
2023-12-27 12:17:37 +0800

    Add load_cell_cycle_genes function to cell_cycle.py

--- tools/sctk/preprocessing/__init__.py
+++ tools/sctk/preprocessing/__init__.py
@@ -9,4 +9,4 @@ from .clustering import leiden_iter, clustree
 from .adata_process import scanpy_pp, layer_pp, split_adata
 from .batch_correct import harmony_integrate
 from .marker_genes import find_marker_genes_in_data, get_rank_genes_from_groups
-from .cell_cycle import cc_genes
+from .cell_cycle import load_cell_cycle_genes
--- tools/sctk/preprocessing/cell_cycle.py
+++ tools/sctk/preprocessing/cell_cycle.py
@@ -1,8 +1,7 @@
 from collections import namedtuple
 
-
 CC_genes = namedtuple('CC_genes', ['s_genes', 'g2m_genes'])
-cc_genes = CC_genes(
+cc_genes_human = CC_genes(
     ['MCM5', 'PCNA', 'TYMS', 'FEN1', 'MCM2', 'MCM4', 'RRM1', 'UNG',
     'GINS2', 'MCM6', 'CDCA7', 'DTL', 'PRIM1', 'UHRF1', 'MLF1IP',
     'HELLS', 'RFC2', 'RPA2', 'NASP', 'RAD51AP1', 'GMNN', 'WDR76',
@@ -19,3 +18,37 @@ cc_genes = CC_genes(
     'AURKA', 'PSRC1', 'ANLN', 'LBR', 'CKAP5', 'CENPE', 'CTCF', 'NEK2',
     'G2E3', 'GAS2L3', 'CBX5', 'CENPA']
 )
+
+# convert by biomaRt
+cc_genes_mouse = CC_genes(
+    ['Mcm4', 'Rrm2', 'Chaf1b', 'Gmnn', 'Uhrf1', 'Mcm2', 'Msh2', 'Cdc45',
+    'Exo1', 'Slbp', 'Rad51ap1', 'Ubr7', 'Hells', 'Cdc6', 'Fen1',
+    'Dscc1', 'Nasp', 'Tyms', 'Wdr76', 'Gins2', 'Dtl', 'Mcm6', 'Tipin',
+    'Ung', 'Usp1', 'Mcm5', 'Pola1', 'Blm', 'Clspn', 'Rpa2', 'Cdca7',
+    'Prim1', 'Ccne2', 'Casp8ap2', 'Brip1', 'Rad51', 'Rrm1', 'E2f8',
+    'Pcna', 'Rfc2'],
+
+    ['Cbx5', 'Gtse1', 'Dlgap5', 'Cdc25c', 'Smc4', 'Ctcf', 'Cdca2',
+    'Kif20b', 'Cdca3', 'Anp32e', 'Cks2', 'Ttk', 'Gas2l3', 'Top2a',
+    'Ncapd2', 'Tacc3', 'Ckap2', 'Hmgb2', 'Cdk1', 'G2e3', 'Tpx2',
+    'Aurkb', 'Lbr', 'Kif11', 'Cenpe', 'Kif2c', 'Kif23', 'Cenpa',
+    'Rangap1', 'Hjurp', 'Cenpf', 'Cks1b', 'Ect2', 'Birc5', 'Ccnb2',
+    'Cdc20', 'Cdca8', 'Hmmr', 'Ndc80', 'Anln', 'Psrc1', 'Nek2',
+    'Nusap1', 'Ube2c', 'Mki67', 'Bub1', 'Aurka', 'Ckap2l', 'Nuf2',
+    'Ckap5', 'Tubb4b']
+)
+
+
+def load_cell_cycle_genes(species: str = 'human') -> tuple:
+    """Load cell cycle genes for human or mouse.
+    Args:
+        species: 'human' or 'mouse'.
+    Returns:
+        A tuple of two lists: (S phase genes, G2M phase genes).
+    """
+    if species == 'human':
+        return cc_genes_human.s_genes, cc_genes_human.g2m_genes
+    elif species == 'mouse':
+        return cc_genes_mouse.s_genes, cc_genes_mouse.g2m_genes
+    else:
+        raise ValueError(f'Unknown species: {species}')
