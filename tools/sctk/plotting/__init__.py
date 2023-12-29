8be3a2fea4d4074b331e1f786fcd52055da90291
WeipengMO <windzlife@gmail.com>
2023-12-29 15:44:04 +0800

    Add custom colormaps to plotting module

--- tools/sctk/plotting/__init__.py
+++ tools/sctk/plotting/__init__.py
@@ -1,2 +1,25 @@
+import matplotlib
+from matplotlib import colors
 from .plotcluster import *
 from .gene_enrich import *
+
+
+class Colormaps():
+    def __init__(self):
+        self.colormaps = {
+            "grey_red": colors.LinearSegmentedColormap.from_list("grey_red", ["lightgray", "red", "darkred"], N=256),
+            "grey_green": colors.LinearSegmentedColormap.from_list("grey_green", ["lightgray", "limegreen", "forestgreen"], N=256),
+            "grey_yellow": colors.LinearSegmentedColormap.from_list("grey_yellow", ["lightgray", "yellow", "gold"], N=256),
+            "grey_violet": colors.LinearSegmentedColormap.from_list("grey_violet", ["lightgray", "mediumvioletred", "indigo"], N=256),
+            "grey_blue": colors.LinearSegmentedColormap.from_list("grey_blue", ["lightgray", "cornflowerblue", "darkblue"], N=256),
+            "magma": colors.LinearSegmentedColormap.from_list("magma", matplotlib.colormaps["magma"].colors+['white'], N=256)
+        }
+
+    def __getitem__(self, key):
+        return self.colormaps.get(key)
+    
+    def __str__(self):
+        return ", ".join(filter(lambda attr: not attr.startswith("__"), self.colormaps))
+
+    def __repr__(self):
+        return self.__str__()
