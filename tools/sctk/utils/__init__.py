d3cc04a001996dc58dd7e720c173f5db2030786b
WeipengMO <windzlife@gmail.com>
2024-04-02 09:22:20 +0800

    Add ensemble_to_symbol function for converting ensemble id to gene symbol

--- tools/sctk/utils/__init__.py
+++ tools/sctk/utils/__init__.py
@@ -1,1 +1,25 @@
 from . import rtools
+import requests
+import json
+
+
+def ensemble_to_symbol(ensemble_id: list):
+    """
+    Convert ensemble id to gene symbol
+
+    Parameters
+    ----------
+    ensemble_id : list
+        List of ensemble id
+    
+    Returns
+    -------
+    output : dict
+        Dictionary of ensemble id and gene symbol
+    """
+    url = "https://biotools.fr/human/ensembl_symbol_converter/"
+    body = {'api': 1, 'ids': json.dumps(ensemble_id)}
+    r = requests.post(url, data = body)
+    output = r.json()
+
+    return output
