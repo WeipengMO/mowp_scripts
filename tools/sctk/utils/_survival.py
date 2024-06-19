b3617d0c873c169ae5b33e6e98f728cf0e710ae4
WeipengMO <windzlife@gmail.com>
2024-06-19 18:43:12 +0800

    feat: change time_limit parameter to group_meta

--- tools/sctk/utils/_survival.py
+++ tools/sctk/utils/_survival.py
@@ -8,6 +8,7 @@ import scanpy as sc
 from matplotlib import pyplot as plt
 import seaborn as sns
 from typing import List, Union
+from loguru import logger
 
 
 class Survival(ad.AnnData):
@@ -65,7 +66,8 @@ class Survival(ad.AnnData):
             groupby: Union[str, List[str]], 
             group_method: Union[str, int] = 'median',
             event: str = 'OS', 
-            time: str = 'OS.time'):
+            time: str = 'OS.time',
+            time_limit: float = None):
         '''
         Group samples based on a given gene signature or a list of genes.
 
@@ -82,18 +84,35 @@ class Survival(ad.AnnData):
         
         time
             The time column in the metadata DataFrame.
+
+        time_limit
+            The time limit to be used for plotting the Kaplan-Meier curve.
         '''
 
         if isinstance(groupby, str):
             survival_data = sc.get.obs_df(self, [groupby, event, time])
-        else:
+        elif isinstance(groupby, list):
             # sc.tl.score_genes(self, groupby)
             # survival_data = sc.get.obs_df(self, ['score', event, time])
 
+            common_genes = list(self.var_names.intersection(groupby))
+            if len(common_genes) == 0:
+                raise ValueError('No common genes are found between the gene signature and the expression data.')
+            genes_to_ignore = set(groupby) - set(common_genes)
+            if len(genes_to_ignore) > 0:
+                logger.info(f'Genes to ignore: {genes_to_ignore}')
+
             # use average expression of a gene signature as a score
-            survival_data = sc.get.obs_df(self, groupby + [event, time])
-            survival_data['score'] = survival_data[groupby].mean(axis=1)
+            survival_data = sc.get.obs_df(self, common_genes + [event, time])
+            survival_data['score'] = survival_data[common_genes].mean(axis=1)
             groupby = 'score'
+        else:
+            raise ValueError('groupby must be a string or a list of strings.')
+        
+        survival_data.dropna(inplace=True)
+
+        if time_limit is not None:
+            survival_data = survival_data[survival_data[time] <= time_limit]
         
         if group_method == 'median': 
             survival_data['group'] = pd.qcut(survival_data[groupby], 2, labels=['Low', 'High'])
@@ -105,7 +124,9 @@ class Survival(ad.AnnData):
             self.group_label = [f'G{i}' for i in range(group_method)]
             survival_data['group'] = pd.qcut(survival_data[groupby], group_method, labels=self.group_label)
 
-        self.obsm['survival'] = survival_data
+        # return survival_data
+
+        self.uns['survival'] = survival_data
         self.event = event
         self.time = time
         self.group_method = group_method
@@ -115,32 +136,30 @@ class Survival(ad.AnnData):
             self, 
             ci_show: bool = False,
             show_censors: bool = False,
-            time_limit: int = None,
             figsize=(3, 3),
             xlabel: str = 'Time',
             ylabel: str = 'Survival probability',
-            pattle = None
+            pattle = None,
+            ax = None
             ):
         if pattle is None:
-            pattle = sns.color_palette(['#377eb8', '#e41a1c', '#984ea3', '#ff7f00'])
+            pattle = sns.color_palette(['#e41a1c', '#377eb8', '#984ea3', '#ff7f00'])
 
         kmf = KaplanMeierFitter()
-        survival_data = self.obsm['survival'][
-            self.obsm['survival']['group'].isin(self.group_label)]
+        survival_data = self.uns['survival'][self.uns['survival']['group'].isin(self.group_label)].copy()
+        survival_data['group'] = survival_data['group'].astype(str)
         
-        survival_data.dropna(inplace=True)
-        
-        if time_limit is not None:
-            survival_data = survival_data[survival_data['OS.time'] <= time_limit]
-        
-        fig, ax = plt.subplots(figsize=figsize)
+        if ax is None:
+            fig, ax = plt.subplots(figsize=figsize)
+        else:
+            fig = ax.get_figure()
 
         by_group = {}
         for (group, _df), color in zip(survival_data.groupby('group'), pattle):
             by_group[group] = _df
             kmf.fit(_df[self.time], _df[self.event], label=group)
-            ax = kmf.plot_survival_function(
-                ci_show=ci_show, show_censors=show_censors, color=color)
+            kmf.plot_survival_function(
+                ci_show=ci_show, show_censors=show_censors, color=color, ax=ax)
 
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
