6917cb489262d4c2e6e2496d222c6f43a1c8b14f
WeipengMO <windzlife@gmail.com>
2024-07-01 14:54:22 +0800

    feat: cluster col and row separately

--- tools/sctk/tl/_clustermap.py
+++ tools/sctk/tl/_clustermap.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram
 import scanpy as sc
+from typing import Union, Literal
 
 
 class ClusterMap:
@@ -12,12 +14,15 @@ class ClusterMap:
     Example:
     --------
     >>> clustermap = sctk.tl.ClusterMap(corr_matrix)
-    >>> clustermap.get_clusters(threshold=.45)
+    >>> clustermap.get_clusters(cluster = 'both', threshold=.45)
     >>> clustermap.show_clusters()
     >>> clustermap.plot(vmax=.5, vmin=-.5)
     '''
 
-    def __init__(self, data: pd.DataFrame, method: str = 'ward'):
+    def __init__(
+            self, 
+            data: pd.DataFrame,
+            ):
         '''
         Initialize a ClusterMap object.
 
@@ -25,21 +30,18 @@ class ClusterMap:
         ----------
         data: pd.DataFrame
             A data matrix to be clustered. Usually a correlation matrix.
-        
-        method: str
-            The linkage method to be used. Default is 'ward'.
         '''
         self.data = data  # corr_matrix
+        self.col_Z = None
+        self.row_Z = None
+        self.col_clusters = None
+        self.row_clusters = None
 
-        Z = linkage(self.data, method=method)
-        self.Z = Z
-    
 
-    def _get_dendrogram(self, threshold=.7, show=True, figsize=(12, 3)):
+    def _get_dendrogram(self, Z, labels, threshold=.7, show=True, figsize=(12, 3), dendrogram_kws=None):
         '''
         Get the dendrogram of the data matrix.
         '''
-        Z = self.Z
         color_threshold = threshold*max(Z[:,2])
 
         if show:
@@ -47,27 +49,62 @@ class ClusterMap:
 
         dendrogram_res = dendrogram(
             Z, 
-            labels=self.data.index, 
+            labels=labels, 
             leaf_rotation=90, 
             color_threshold=color_threshold,
-            no_plot=not show)
-        self.dendrogram_res = dendrogram_res
+            no_plot=not show,
+            **(dendrogram_kws if dendrogram_kws is not None else {})
+        )
+        
+        return dendrogram_res
         
 
-    def get_clusters(self, threshold=.7, show=True, figsize=(12, 3)):
+    def get_clusters(
+            self, 
+            cluster: Literal['col', 'row', 'both'],
+            method: str = 'ward',
+            threshold=.7,
+            show=True, 
+            figsize=(12, 3),
+            dendrogram_kws=None):
         '''
         Get the clusters based on the dendrogram.
 
         Parameters:
         ----------
+        cluster: str
+            The type of cluster to be performed. Either 'col', 'row', or 'both'.
+
+        method: str
+            The linkage method to be used. Default is 'ward'.
+
         threshold: float
             The threshold for cutting the dendrogram. Default is 0.7.
         
         show: bool
             Whether to show the dendrogram. Default is True.
+        
+        figsize: tuple
+            The size of the figure. Default is (12, 3).
+        
+        dendrogram_kws: dict
+            Additional keyword arguments for the `scipy dendrogram` function.
         '''
-        self._get_dendrogram(threshold=threshold, show=show, figsize=figsize)
-        dendrogram_res = self.dendrogram_res
+        if  cluster == 'both':
+            assert self.data.shape[0] == self.data.shape[1], 'The data matrix must be square for both row and column clustering'
+            Z = linkage(self.data, method=method)
+            labels = self.data.columns
+        elif cluster == 'col':
+            Z = linkage(self.data.T, method=method)
+            labels = self.data.columns
+        elif cluster == 'row':
+            Z = linkage(self.data, method=method)
+            labels = self.data.index
+        else:
+            raise ValueError(f'cluster must be either "col", "row", or "both"')
+        
+        dendrogram_res = self._get_dendrogram(
+            Z, labels=labels, threshold=threshold, show=show, figsize=figsize, dendrogram_kws=dendrogram_kws)
         
         idx = 0
         last_c = None
@@ -83,12 +120,29 @@ class ClusterMap:
                 last_c = c
         clusters= pd.Series(cluster_idx, dendrogram_res['ivl'])
 
-        self.clusters = clusters
+        if cluster == 'col':
+            self.col_clusters = clusters
+            self.col_Z = Z
+        elif cluster == 'row':
+            self.row_clusters = clusters
+            self.row_Z = Z
+        elif cluster == 'both':
+            self.col_clusters = clusters
+            self.col_Z = Z
+            self.row_clusters = clusters
+            self.row_Z = Z
 
 
-    def show_clusters(self):
+    def show_clusters(self, cluster = 'col'):
+        if cluster == 'col':
+            clusters = self.col_clusters
+        elif cluster == 'row':
+            clusters = self.row_clusters
+        else:
+            raise ValueError(f'cluster must be either "col" or "row"')
+        
         cluster = {}
-        for k, v in self.clusters.items():
+        for k, v in clusters.items():
             if v not in cluster:
                 cluster[v] = []
             cluster[v].append(k)
@@ -96,8 +150,14 @@ class ClusterMap:
         return cluster
 
     
-    def _get_cluster_color(self, cluster_pal: list = None):
-        clusters = self.clusters
+    def _get_cluster_color(self, cluster = 'col', cluster_pal: list = None):
+        if cluster == 'col':
+            clusters = self.col_clusters
+        elif cluster == 'row':
+            clusters = self.row_clusters
+        else:
+            raise ValueError(f'cluster must be either "col" or "row"')
+        
         data = self.data
 
         clusters_color_unique = clusters.unique()
@@ -115,77 +175,133 @@ class ClusterMap:
         
         cluster_lut = dict(zip(clusters_color_unique, cluster_pal))
         cluster_color = clusters.map(cluster_lut)
-        cluster_color = cluster_color[data.index]
+        if cluster == 'col':
+            cluster_color = cluster_color[data.columns]
+        elif cluster == 'row':
+            cluster_color = cluster_color[data.index]
 
         return cluster_color.values
 
 
+    def _handle_colors(self, colors, cluster_type, data_dimension, cluster_pal):
+        cluster_color = self._get_cluster_color(
+            cluster=cluster_type, cluster_pal=cluster_pal) if getattr(self, f"{cluster_type}_clusters") is not None else None
+        
+        if colors is False:
+            return None
+        elif colors is None:
+            return cluster_color
+        elif isinstance(colors, (pd.Series, list, np.ndarray)):
+            if len(colors) == data_dimension:
+                if cluster_color is None:
+                    return colors
+                else:
+                    return [colors, cluster_color]
+            else:
+                for i, c in enumerate(colors):
+                    if len(c) != data_dimension:
+                        raise ValueError(f'{cluster_type}_colors[{i}] has different length with data')
+                if cluster_color is None:
+                    return colors
+                else:
+                    return colors + [cluster_color]
+        else:
+            return colors
+
+
     def plot(
-            self, 
-            col_colors: list=None, 
-            cmap='RdBu_r',
-            vmax=None, vmin=None,
-            dendrogram_ratio=0.1,
-            cluster_pal: list =None,
-            figsize=(5, 5)):
+        self, 
+        col_colors: Union[list, Literal[False]] = None, 
+        row_colors: Union[list, Literal[False]] = None,
+        cmap='RdBu_r',
+        vmax: float = None, 
+        vmin: float = None,
+        center: float = None,
+        dendrogram_ratio: Union[float, tuple] = 0.1,
+        col_cluster_pal: list = None,
+        row_cluster_pal: list = None,
+        show_row_dendrogram: bool = False,
+        show_col_dendrogram: bool = False,
+        xticklabels=False,
+        yticklabels=False,
+        x_rotation: float = None,
+        y_rotation: float = None,
+        figsize: tuple = (5, 5)
+    ):
         '''
         Plot the cluster map.
 
         Parameters:
         ----------
-        col_colors: list
-            A list of colors for the columns. Default is None.
-        
-        cmap: str
-            The colormap to be used. Default is 'RdBu_r'.
-        
-        vmax: float
-            The maximum value for the colormap. Default is None.
-        
-        vmin: float
-            The minimum value for the colormap. Default is None.
-        
-        dendrogram_ratio: float
-            The ratio of the dendrogram. Default is 0.1.
-
-        cluster_pal: list
-            A list of colors for the clusters. Default is None.
-        
-        figsize: tuple
+        col_colors : list, optional
+            List of colors for each column, used to color the column labels. if False, no color will be used.
+        row_colors : list, optional
+            List of colors for each row, used to color the row labels. if False, no color will be used.
+        cmap : str, optional
+            The colormap to use for the heatmap. Default is 'RdBu_r'.
+        vmax : float, optional
+            The maximum value to use for the colormap. Default is None.
+        vmin : float, optional
+            The minimum value to use for the colormap. Default is None.
+        dendrogram_ratio : float, optional
+            The ratio of the dendrogram height to the heatmap height. Default is 0.1.
+        col_cluster_pal : list, optional
+            List of colors for the column dendrogram clusters. Default is None.
+        row_cluster_pal : list, optional
+            List of colors for the row dendrogram clusters. Default is None.
+        show_row_dendrogram : bool, optional
+            Whether to show the row dendrogram. Default is False.
+        show_col_dendrogram : bool, optional
+            Whether to show the column dendrogram. Default is False.
+        figsize : tuple, optional
             The size of the figure. Default is (5, 5).
+
+        Returns:
+        -------
+        g : seaborn.ClusterGrid
+            The cluster grid object representing the plot.
+
         '''
         
-        Z = self.Z
-        cluster_color = self._get_cluster_color(cluster_pal=cluster_pal)
+        # Usage for col_colors
+        self.col_colors = self._handle_colors(col_colors, 'col', len(self.data.columns), col_cluster_pal)
 
-        if col_colors is None:
-            col_colors = cluster_color
-        else:
-            if isinstance(col_colors, (pd.Series, list)):
-                if len(col_colors) == len(self.data.index):
-                    col_colors = [col_colors, cluster_color]
-                else:
-                    for i, c in enumerate(col_colors):
-                        if len(c) != len(self.data.index):
-                            raise ValueError(f'col_colors[{i}] has different length with data')
-                    col_colors.append(cluster_color)
+        # Usage for row_colors
+        self.row_colors = self._handle_colors(row_colors, 'row', len(self.data.index), row_cluster_pal)
+
+        col_cluster = False if self.col_Z is None else True
+        row_cluster = False if self.row_Z is None else True
 
         g = sns.clustermap(
             self.data, 
-            row_linkage=Z, 
-            col_linkage=Z,
+            row_linkage=self.row_Z, 
+            col_linkage=self.col_Z,
             cmap=cmap,
-            col_colors=col_colors,
-            xticklabels=False, 
-            yticklabels=False,
+            row_cluster = row_cluster,
+            col_cluster = col_cluster,
+            col_colors=self.col_colors,
+            row_colors=self.row_colors,
+            xticklabels=xticklabels, 
+            yticklabels=yticklabels,
             cbar_kws=dict(orientation='horizontal'),
             dendrogram_ratio=dendrogram_ratio,
-            figsize=figsize, vmax=vmax, vmin=vmin)
-
-        g.ax_row_dendrogram.set_visible(False) 
-        # g.ax_col_dendrogram.set_visible(False)
+            figsize=figsize, 
+            vmax=vmax, 
+            vmin=vmin,
+            center=center)
+        
+        if not show_row_dendrogram:
+            g.ax_row_dendrogram.set_visible(False)
+        
+        if not show_col_dendrogram:
+            g.ax_col_dendrogram.set_visible(False)
 
         heatmap_bbox = g.ax_heatmap.get_position()
         g.ax_cbar.set_position([heatmap_bbox.xmax-heatmap_bbox.width/5, -heatmap_bbox.ymin, heatmap_bbox.width/5, 0.02])
+
+        if x_rotation is not None:
+            g.ax_heatmap.set_xticklabels(g.ax_heatmap.get_xticklabels(), rotation=x_rotation, ha='right')
+        if y_rotation is not None:
+            g.ax_heatmap.set_yticklabels(g.ax_heatmap.get_yticklabels(), rotation=y_rotation, ha='right')
 
         return g
