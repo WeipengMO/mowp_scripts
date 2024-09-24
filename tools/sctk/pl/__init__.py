ee58081a572b5f899138448ee995dc153c798667
WeipengMO <windzlife@gmail.com>
2024-09-24 17:19:40 +0800

    feat: Add ccc_dotplot function for visualizing cell-cell communication network
    - The function generates a dot plot visualization of interactions between ligands and receptors

--- tools/sctk/pl/__init__.py
+++ tools/sctk/pl/__init__.py
@@ -8,4 +8,4 @@ from .enhanced_scanpy import (
     violinplot)
 from ._color import ColorMaps, ColorPalette
 from ._dotplot import DotPlot
-from ._cell_cell_communication import circle_plot
+from ._cell_cell_communication import circle_plot, ccc_dotplot
--- tools/sctk/pl/_cell_cell_communication.py
+++ tools/sctk/pl/_cell_cell_communication.py
@@ -39,12 +39,14 @@ def pivot_liana_res(
     return pivot_table
 
 
-def scale_list(arr, min_val=1, max_val=5):
+def scale_list(arr, min_val=1, max_val=5, keep_zero=True):
     arr = np.array(arr)
     arr_min = np.min(arr)
     arr_max = np.max(arr)
 
     scaled_arr = (arr - arr_min) / (arr_max - arr_min) * (max_val - min_val) + min_val
+    if keep_zero:
+        scaled_arr[arr == 0] = 0  # Set the value of 0 to 0
     
     return scaled_arr
 
@@ -75,6 +77,27 @@ def get_mask_df(
             mask_df.loc[source_cell_type, target_cell_type] = True
 
     return pivot_table[mask_df].fillna(0)
+
+
+def align_dataframe(df):
+    # 获取行名和列名
+    row_names = set(df.index)
+    col_names = set(df.columns)
+    
+    # 找出行名中缺少的列名
+    missing_rows = col_names - row_names
+    for row in missing_rows:
+        df.loc[row] = [0] * len(df.columns)
+    
+    # 找出列名中缺少的行名
+    missing_cols = row_names - col_names
+    for col in missing_cols:
+        df[col] = [0] * len(df.index)
+    
+    # 重新排序行和列
+    df = df.sort_index().sort_index(axis=1)
+    
+    return df
 
 
 def circle_plot(
@@ -166,6 +189,7 @@ def circle_plot(
         target=target_key, 
         values=values_key, 
         mode=pivot_mode)
+    pivot_table = align_dataframe(pivot_table)
 
     cell_type_colors = get_adata_color(adata, label=cell_type_key)
 
@@ -185,7 +209,7 @@ def circle_plot(
 
     node_color = [cell_type_colors[cell] for cell in G.nodes]
     node_size = pivot_table.sum(axis=1).values
-    node_size = scale_list(node_size, max_val=node_size_scale[1], min_val=node_size_scale[0])
+    node_size = scale_list(node_size, max_val=node_size_scale[1], min_val=node_size_scale[0], keep_zero=False)
 
 
     fig, ax = plt.subplots(figsize=figure_size)
@@ -229,3 +253,227 @@ def circle_plot(
     ax.set_aspect('equal')
 
     return ax
+
+
+def ccc_dotplot_p9(
+    liana_res, 
+    colour,  # colour='lr_means'
+    size, # size='cellphone_pvals'
+    color_range: tuple = None,  # vmax, vmin (0, .32)
+    size_range: tuple = None,  # 0.5, 5
+    figsize: tuple = None,  # (7, 4)
+    cmap_title: str = None,
+    size_title: str = None,
+    return_fig: bool = True,
+    sort_interaction: Literal['target', 'source'] = None,
+    interaction_order: list = None,
+    cmap='RdBu_r',
+    ):
+    '''
+    Generate a dot plot visualization of interactions between ligands and receptors.
+
+    Parameters:
+    -----------
+    liana_res : DataFrame
+        The dataframe containing the interaction data.
+    colour : str
+        The column name in `liana_res` to use for coloring the dots.
+    size : str
+        The column name in `liana_res` to use for sizing the dots.
+    color_range : tuple, optional
+        The range of values for the color scale. Default is None.
+    size_range : tuple, optional
+        The range of values for the size scale. Default is None.
+    figsize : tuple, optional
+        The size of the figure. Default is None.
+    cmap_title : str, optional
+        The title for the color scale. Default is None.
+    size_title : str, optional
+        The title for the size scale. Default is None.
+    '''
+    try:
+        from plotnine import ggplot, geom_point, aes, \
+            facet_grid, labs, theme_bw, theme, element_text, element_rect, scale_size_continuous, scale_color_cmap
+    except ImportError:
+        logger.error('Please install plotnine to use this function.')
+        return
+
+    if cmap_title is None:
+        cmap_title = colour,
+    if size_title is None:
+        size_title = size
+
+    if sort_interaction == 'target':
+        liana_res = liana_res.copy()
+        interaction_cat = sorted(set(liana_res['interaction']), key=lambda x: x.split(' -> ')[1])
+        liana_res["interaction"] = pd.Categorical(liana_res["interaction"], categories=interaction_cat)
+    elif sort_interaction == 'source':
+        liana_res = liana_res.copy()
+        interaction_cat = sorted(set(liana_res['interaction']), key=lambda x: x.split(' -> ')[0])
+        liana_res["interaction"] = pd.Categorical(liana_res["interaction"], categories=interaction_cat)
+    elif interaction_order is not None:
+        liana_res = liana_res.copy()
+        liana_res["interaction"] = pd.Categorical(liana_res["interaction"], categories=interaction_order)
+
+    p = (ggplot(liana_res, aes(x='target', y='interaction', colour=colour, size=size))
+            + geom_point()
+            + facet_grid('~source')
+            + scale_size_continuous(range=size_range)
+            + scale_color_cmap(cmap, limits=color_range)
+            + labs(color=cmap_title,
+                    size=size_title,
+                    y="Interactions (Ligand → Receptor)",
+                    x="Target",
+                    title="Source")
+            + theme_bw(base_size=10)
+            + theme(
+                # legend_text=element_text(size=14),
+                # strip_background=element_rect(fill="white"),
+                # strip_text=element_text(size=15, colour="black"),
+                # axis_text_y=element_text(size=10, colour="black"),
+                # axis_title_y=element_text(colour="#808080", face="bold", size=15),
+                axis_text_x=element_text(angle=30, ha='right'),
+                figure_size=figsize,
+                plot_title=element_text(hjust=0.5, size=10)
+                )
+        )
+    
+    if return_fig:
+        return p
+    
+    p.draw()
+
+
+def ccc_dotplot(
+        data: pd.DataFrame,
+        source: str = 'source',
+        target: str = 'target',
+        source_target: str = 'source:target',
+        interaction: str = 'interaction',
+        source_color: str = 'Set1',
+        target_color: str = 'Dark2',
+        legend_kws: dict = {'fontsize': 8, 'frameon': False},
+        sort_interaction: Union[list, str] = ['receptor_complex', 'ligand_complex'],
+        sort_interaction_accending: bool = True,
+        figsize: tuple = (4, 8),
+        swap_axes: bool = False,
+        **kwargs,
+    ):
+    '''
+    Generate a dot plot visualization of interactions between ligands and receptors.
+
+    Parameters:
+    -----------
+    data : DataFrame
+        The dataframe containing the interaction data.
+    source : str
+        The column name of the source cell type.
+    target : str
+        The column name of the target cell type.
+    source_target : str
+        The column name of the source:target pair.
+    interaction : str
+        The column name of the interaction.
+    source_color : str
+        The color map for the source cell type.
+    target_color : str
+        The color map for the target cell type.
+    legend_kws : dict
+        The keyword arguments for the legend.
+    sort_interaction : Union[list, str]
+        The column name(s) to sort the interactions. Default is ['receptor_complex', 'ligand_complex'].
+    sort_interaction_accending : bool
+        Whether to sort the interactions in ascending order. Default is True.
+    figsize : tuple
+        The size of the figure. Default is (4, 8).
+    swap_axes : bool
+        Whether to swap the axes. Default is False.
+    '''
+    try:
+        from PyComplexHeatmap import HeatmapAnnotation, DotClustermapPlotter, anno_simple, anno_label
+    except ImportError:
+        logger.error('Please install PyComplexHeatmap to use this function.')
+        return
+
+    if isinstance(sort_interaction, str):
+        sort_interaction = [sort_interaction]
+
+    
+    if not swap_axes:
+
+        # prepare column annotation
+        df_col = data[[source, target, source_target]].drop_duplicates().set_index(source_target)
+
+        col_ha = HeatmapAnnotation(
+            Source=anno_simple(df_col[source], cmap=source_color, legend=True, add_text=False, legend_kws=legend_kws), 
+            Target=anno_simple(df_col[target], cmap=target_color, legend=True, add_text=False, legend_kws=legend_kws),
+            verbose=0,
+            label_side='left',
+            label_kws={'horizontalalignment':'right', 'fontsize': 9},
+            axis=1,
+            hgap=.75,
+        )
+
+        plt.figure(figsize=figsize)
+        dm = DotClustermapPlotter(
+            data=data, 
+            x=source_target, y=interaction, # hue='target', 
+            value='lr_means',c='lr_means',s='cellphone_pvals',
+            row_cluster=False,
+            col_cluster=False,
+            cmap='RdBu_r', vmin=0, vmax=.5,
+            top_annotation=col_ha,
+
+            # Interactions (ligand -> receptor)
+            show_rownames=True,
+            row_names_side='left',
+            yticklabels_kws={'labelsize': 6},
+
+            y_order=data.sort_values(sort_interaction, ascending=sort_interaction_accending)['interaction'].drop_duplicates().to_list(),
+
+            verbose=0,
+            legend_gap=7,
+            # spines=False,
+            # grid='minor',
+            **kwargs,
+        )
+    
+    else:
+        # prepare row annotation
+        df_row = data[[source, target, source_target]].drop_duplicates().set_index(source_target)
+
+        row_ha = HeatmapAnnotation(
+            Source=anno_simple(df_row[source], cmap=source_color, legend=True, add_text=False, legend_kws=legend_kws), 
+            Target=anno_simple(df_row[target], cmap=target_color, legend=True, add_text=False, legend_kws=legend_kws),
+            verbose=0,
+            label_side='top',
+            label_kws={'horizontalalignment':'left', 'fontsize': 8, 'rotation': 60},
+            axis=0,
+            wgap=1,
+        )
+
+        plt.figure(figsize=figsize)
+        dm = DotClustermapPlotter(
+            data=data, 
+            x=interaction, y=source_target, # hue='target', 
+            value='lr_means',c='lr_means',s='cellphone_pvals',
+            row_cluster=False,
+            col_cluster=False,
+            cmap='RdBu_r', vmin=0, vmax=.5,
+            left_annotation=row_ha,
+
+            # Interactions (ligand -> receptor)
+            show_colnames=True,
+            col_names_side='bottom',
+            xticklabels_kws={'labelsize': 6, 'labelrotation': 30},
+
+            x_order=data.sort_values(sort_interaction, ascending=sort_interaction_accending)['interaction'].drop_duplicates().to_list(),
+
+            verbose=0,
+            legend_gap=7,
+            # spines=False,
+            # grid='minor',
+            **kwargs,
+        )
+
+    return dm
